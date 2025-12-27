e9d04602042f876e7609bb222e1116636572a66e
kumardeepakg92 <kumardeepakg92@gmail.com>
2025-12-27 23:50:55 +0530

    space

--- inputWithSource.sh
+++ inputWithSource.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
 source /mnt/c/Users/dpkjs/Documents/GITHUB/devopswithdeepak/CommonVars
-
 echo "Container tool is : ${container}"
 echo "Configuration management tool is : ${cmtools}"
